7ec5233d6285b4b5546ed7db66b35aa5e803a262
Diwash Rai <diwash.rai1993@gmail.com>
2024-03-31 17:16:33 +0100

    Base implementation from book complete

--- sphere.h
+++ sphere.h
@@ -9,6 +9,9 @@ class sphere : public hittable
 public:
     sphere(point3 _center, double _radius) : center(_center), radius(_radius) {};
 
+    sphere(point3 _center, double _radius, shared_ptr<material> _material)
+        : center(_center), radius(_radius), mat(std::move(_material)) {};
+
     bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override
     {
         vec3 oc = r.origin() - center;
@@ -59,6 +62,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -66,6 +70,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif //RAYTRACING_SPHERE_H
